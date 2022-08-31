9e9b9959c30d12a2aad946f36e94e077ebf651e4
Ramya <107109350+RamyaDasari373@users.noreply.github.com>
2022-08-31 17:57:32 +0530

    uploaded main method class file

---
+++ Emp_Management/ExployeeManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExployeeManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExployeeManagementApiApplication.class, args);
+	}
+
+}
